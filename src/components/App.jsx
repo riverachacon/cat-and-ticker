9c09e9dd258b62b94d019d5acb9ca740746fbd5f
Giovanni Rivera Chacon <princecito@Giovannis-Air.lan>
2024-07-12 19:58:30 -0400

    App style & component update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import CatImg from "./CatImg";
 import TickerInfo from "./TickerInfo";
+import TickerNews from "./TickerNews";
 
 function App() {
+  const [ticker, setTicker] = useState("");
+  useEffect(() => {
+    console.log(ticker); // log current ticker
+  }, [ticker]);
+
+  function searchTicker(newTicker) {
+    setTicker(newTicker);
+  }
   return (
     <>
       <CatImg />
-      <TickerInfo />
+      <TickerInfo onSearchTicker={searchTicker} />
+      <TickerNews onticker={ticker} />
+      {/**getting ticker from component */}
     </>
   );
 }
