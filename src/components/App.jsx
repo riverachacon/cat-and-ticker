cd97b69fb0ef96ee5b73b1c29d1df420a2bdf30d
Giovanni Rivera Chacon <princecito@Giovannis-Air.lan>
2024-07-18 17:56:58 -0400

    fix date1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,29 @@ import TickerNews from "./TickerNews";
 
 function App() {
   const [ticker, setTicker] = useState("");
+  const [date, setDate] = useState("");
+
   useEffect(() => {
     console.log(ticker); // log current ticker
-  }, [ticker]);
+    console.log(date);
+  }, [ticker, date]);
 
   function searchTicker(newTicker) {
     setTicker(newTicker);
   }
+  function handleDate(date) {
+    let day = date.$D.toString().padStart(2, "0"); // single-digit days and months are prefixed with a zero
+    let month = (date.$M + 1).toString().padStart(2, "0"); // +1 because months are 0-indexed
+    let year = date.$y.toString();
+    let convertedDate = `${year}-${month}-${day}`;
+    setDate(convertedDate);
+    // console.log(date);
+  }
   return (
     <>
       <CatImg />
-      <TickerInfo onSearchTicker={searchTicker} />
-      <TickerNews onticker={ticker} />
+      <TickerInfo onDateSelected={handleDate} onSearchTicker={searchTicker} />
+      <TickerNews onDate={date} onTicker={ticker} />
       {/**getting ticker from component */}
     </>
   );
